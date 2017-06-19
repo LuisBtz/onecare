60b3117c6ec1426b8560d329c68c2c98ac9eed2f
Luis B <luisbttf@gmail.com>
2017-06-19 15:41:05 -0500

    asd
    asd

---
+++ assets/js/functions.js
@@ -0,0 +1,11 @@
+const $navbar = $('.navbar');
+
+$('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    const scrollTop =
+        $($(this).attr('href')).position().top -
+        $navbar.outerHeight();
+
+    $('html, body').animate({ scrollTop });
+})
